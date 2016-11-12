7339b2bb7b3327200bbf819c533b341ba997fead
RinesThaix <iam@kostya.sexy>
2016-11-12 07:45:14 +0300

    Fix for /lc

--- src/main/java/ru/luvas/dk/server/command/commands/ListClusters.java
+++ src/main/java/ru/luvas/dk/server/command/commands/ListClusters.java
@@ -17,16 +17,16 @@ public class ListClusters extends Command {
 
     @Override
     public void handle(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("Все существующие кластеры:");
         int page = 1;
         if(args.length == 1)
             try {
                 page = Integer.parseInt(args[0]);
             }catch(NumberFormatException ex) {}
+        List<String> list = new ArrayList<>();
+        list.add("Все существующие кластеры (страница " + page + "):");
         List<String> clusters = new ArrayList<>(DarlingKate.getClassifier().getAllClustersNames());
         final int perPage = 20;
-        for(int i = (page - 1) * perPage; i < page * perPage; ++i)
+        for(int i = (page - 1) * perPage; i < Math.min(page * perPage, clusters.size()); ++i)
             list.add("- " + clusters.get(i));
         sender.sendMessage(list);
     }
